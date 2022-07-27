95b23d34fcf60c53d56bf22fa80e4fcc46369976
David <workingnown@gmail.com>
2022-07-26 21:49:25 -0300

    Método all() e race() - sua usabilidade no código

--- Promises/scripty.js
+++ Promises/scripty.js
@@ -29,3 +29,45 @@ promisseRetornoDoCatch.then((data) => {
 }).catch((err) => {
     console.log('Aconteceu um erro: ' + err)
 })
+
+// Resolver várias promessas com all()
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('P1 Ok! Timeout')
+    }, 2500)
+})
+const p2 = new Promise((resolve, reject) => {
+    resolve('P2 ok!')
+})
+const p3 = new Promise((resolve, reject) => {
+    resolve('P3 ok!')
+})
+
+// As Promessas p2 e p3 aguardam a Promessa p1 ser resovilda e em seguida executadas.
+
+const resolveAll = Promise.all([p1, p2, p3]).then((data) => {
+    console.log(data)
+})
+
+console.log('Depois do all()')
+
+// Várias Promessas com race()
+
+const p4 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('P4 Ok! Timeout')
+    }, 2500)
+})
+const p5 = new Promise((resolve, reject) => {
+    resolve('P5 ok!')
+})
+const p6 = new Promise((resolve, reject) => {
+    resolve('P6 ok!')
+})
+
+// As Promessas p5 e p6 aguardam a Promessa p4 ser resovilda e em seguida executadas.
+
+const resolveAllRace = Promise.race([p4, p5, p6]).then((data) => {
+    console.log(data)
+})
