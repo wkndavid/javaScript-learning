effb9c308ba242b362420d7363c36ea5656e411d
David <workingnown@gmail.com>
2022-07-27 22:48:53 -0300

    Função Sícrona - Contexto Promise

---
+++ Promises/CódigoFonte/script.js
@@ -0,0 +1,21 @@
+// Função Síncrona
+// Chamando funções externas
+function sumNumbers() {
+    let result = 1 + 1
+
+    if (result == 2) {
+        sucessCallback()
+    } else {
+        errorCallback()
+    }
+}
+
+function sucessCallback() {
+    console.log('Yeah! Number 2!')
+}
+
+function errorCallback() {
+    console.log('Oops! Something wrong.')
+}
+
+sumNumbers();
