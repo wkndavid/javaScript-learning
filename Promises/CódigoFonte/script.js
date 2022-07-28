ac6358412bc9f94e26c941558d930bea9339aa79
David <workingnown@gmail.com>
2022-07-27 22:52:48 -0300

    Pricípio básico das Promises

--- Promises/CódigoFonte/script.js
+++ Promises/CódigoFonte/script.js
@@ -19,3 +19,15 @@ function errorCallback() {
 }
 
 sumNumbers();
+
+// Promise {}
+
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 1
+    if (a == 2) {
+        resolve('Sucess!')
+    } else {
+        reject('Failed!')
+    }
+})
+
