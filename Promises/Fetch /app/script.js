7f58ac48d42915dcb12630970b9744fad95c3c9d
David <workingnown@gmail.com>
2022-08-04 09:14:56 -0300

    Carregamento via Fetch

--- Promises/Fetch /app/script.js
+++ Promises/Fetch /app/script.js
@@ -1,7 +1,38 @@
-const response = fetch('https://jsonplaceholder.typicode.com/posts')
-.then(function(responseData){
-   return responseData.json();
-})
-.then(function(jsonData) {
-    console.log(jsonData);
-});
+const url = 'https://jsonplaceholder.typicode.com/posts'
+
+const loadingElement = document.querySelector('#loading')
+const postContainer = document.querySelector('#post-container')
+
+// Pegando todos os POSTS com async / await
+
+async function getAllPosts() {
+
+    const response = await fetch(url);
+
+    console.log(response);
+
+    const data = await response.json();
+    console.log(data);
+
+    loadingElement.classList.add('hide');
+
+    data.map((post) => {
+        const div = document.createElement('div');
+        const title = document.createElement('h2');
+        const body = document.createElement('p');
+        const link = document.createElement('a');
+
+        title.innerText = post.title;
+        title.innerText = post.body;
+        link.innerText = 'Ler';
+        link.setAttribute('href', `/post.html?id=${post.id}`);
+
+        div.appendChild(title);
+        div.appendChild(body);
+        div.appendChild(link);
+
+        postContainer.appendChild(div);
+    });
+}
+
+getAllPosts();
