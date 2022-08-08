c9e36d5af98d2d591bfc0887f0e4976b5ce08243
David <workingnown@gmail.com>
2022-08-08 09:31:01 -0300

    Hoisting

---
+++ Hoisting/hoisting.js
@@ -0,0 +1,10 @@
+var a = 2;
+console.log(a);
+
+function qualquer() {
+    console.log(a);
+    var a = 3;
+    console.log(a);
+}
+
+qualquer();
--- Promises/Fetch /app/script.js
+++ Promises/Fetch /app/script.js
@@ -1,7 +1,15 @@
-const url = 'https://jsonplaceholder.typicode.com/posts'
+const url = 'https://api.nasa.gov/planetary/apod?api_key=kjGSZtnFbwhhAuThWke37d5onXeOIeMtzpRfPx2b';
 
-const loadingElement = document.querySelector('#loading')
-const postContainer = document.querySelector('#post-container')
+const loadingElement = document.querySelector('#loading');
+const postContainer = document.querySelector('#posts-container');
+
+const postPage = document.querySelector('#posts');
+const postsContainer = document.querySelector('#posts-container');
+const commentsContainer = document.querySelector('#comments-container');
+
+// Pegando id da url 
+const urlSearch = new URLSearchParams(document.location.search);
+const postId = urlSearch.get('id');
 
 // Pegando todos os POSTS com async / await
 
@@ -16,23 +24,30 @@ async function getAllPosts() {
 
     loadingElement.classList.add('hide');
 
-    data.map((post) => {
+    data.map((posts) => {
         const div = document.createElement('div');
         const title = document.createElement('h2');
         const body = document.createElement('p');
         const link = document.createElement('a');
 
-        title.innerText = post.title;
-        title.innerText = post.body;
+        title.innerText = posts.title;
+        title.innerText = posts.body;
         link.innerText = 'Ler';
-        link.setAttribute('href', `/post.html?id=${post.id}`);
+        link.setAttribute('href', `/post.html?id=${posts.id}`);
 
         div.appendChild(title);
         div.appendChild(body);
         div.appendChild(link);
 
-        postContainer.appendChild(div);
+        postsContainer.appendChild(div);
     });
 }
+// Post Id
 
-getAllPosts();
+if (!postId) {
+    getAllPosts(); 
+} else {
+    console.log(postId)
+}
+
+console.log(urlSearch)
