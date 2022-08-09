9f788d57e233346a7d7f9331a444246d97c73832
David <workingnown@gmail.com>
2022-08-09 02:37:44 -0300

    Operação assíncrona e promises

--- Promises/rogermelo.js
+++ Promises/rogermelo.js
@@ -1,11 +1,23 @@
 const url = 'https://dog.ceo/api/breeds/image/random'
+const dogImg = document.querySelector('[data-js="dog-img"]')
+console.log(dogImg)
+
+const validateHTTPStatus = dogData => {
+    if (!dogData.ok) {
+        throw new Error(`HTTP error, status ${dogData.status}`)
+    }
+    return dogData.json()
+}
+
+const setDogImg = ({ message: url }) => {
+    dogImg.setAttribute('src', url)
+}
+
+const handleRequestError = error => {
+    console.log(error.message)
+}
 
 fetch(url)
-    .then((dogData) => {
-        if (dogData.ok) {
-            throw new Error(`HTTP error, status ${dogData.status}`)
-        }
-    })
-    .catch((error) => {
-        console.log(error.message)
-    })
+    .then(validateHTTPStatus)
+    .then(setDogImg)
+    .catch(handleRequestError)
